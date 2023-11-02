bac6e0283d298a663f503715cdfc87663161fe90
Toni Lukkaroinen <tmkcodes@gmx.com>
2023-11-02 06:58:53 +0200

    Update windows update script to actually copy the files.

--- update-windows.bat
+++ update-windows.bat
@@ -8,22 +8,25 @@ set ZIP_FILE=hoobot-latest.zip
 REM Create a backup of .env file
 if exist .env (
     copy .env .env_backup >nul
-    echo .env file backed up as .env_backup
+    echo .env file backed up
 )
 
 :: Step 1: Download the zip folder
 powershell -command "& { (New-Object Net.WebClient).DownloadFile('%ZIP_URL%', '%ZIP_FILE%') }"
 
 :: Step 2: Create tmp directory if it doesn't exist
-mkdir %TMP_DIR%
+if not exist "%TMP_DIR%" mkdir "%TMP_DIR%"
 
 :: Step 3: Extract the zip folder to tmp
 powershell -command "& { Expand-Archive -Path '%ZIP_FILE%' -DestinationPath '%TMP_DIR%' }"
 
-:: Step 4: Overwrite the working directory with the extracted files
-xcopy /Y %TMP_DIR%\* .\
+:: Step 4: Move the directory
+move "%TMP_DIR%\Hoobot-*" "%TMP_DIR%\hoobot"
 
-:: Step 5: Remove the downloaded zip file and tmp folder
+:: Step 5: Copy the files
+xcopy /Y /S /E "%TMP_DIR%\hoobot\*" .\
+
+:: Step 6: Remove the downloaded zip file and tmp folder
 del %ZIP_FILE%
 rmdir /S /Q %TMP_DIR%
 
