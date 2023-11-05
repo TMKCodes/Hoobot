b28bd3cb48cc2aba11156c50089853e175d9a8ae
Toni Lukkaroinen <tmkcodes@gmx.com>
2023-11-05 12:29:47 +0200

    Add audio disabling.

--- src/Hoobot/Utilities/playSound.ts
+++ src/Hoobot/Utilities/playSound.ts
@@ -29,15 +29,19 @@ import playSound from 'play-sound';
 
 export const play = (file: string): Promise<void> => {
   return new Promise((resolve, reject) => {
-    const player = playSound({});
-    player.play(file, { mplayer: ['-volume', 100] }, (error: any) => {
-      if (error) {
-        console.error('Error playing sound:', error);
-        reject(error);
-      } else {
-        console.log('Sound is playing...');
-        resolve();
-      }
-    });
+    if (process.env.PLAY_AUDIO !== "false") {
+      const player = playSound({});
+      player.play(file, { mplayer: ['-volume', 100] }, (error: any) => {
+        if (error) {
+          console.error('Error playing sound:', error);
+          reject(error);
+        } else {
+          console.log('Sound is playing...');
+          resolve();
+        }
+      });
+    } else {
+      resolve();
+    }
   });
 }
