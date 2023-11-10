45d30a7101254911f5a63c1b53cb519c2d8c9d51
Toni Lukkaroinen <tmkcodes@gmx.com>
2023-11-10 10:55:37 +0200

    Fix handleOpenOrders to one function.

--- src/Hoobot/Binance/orders.ts
+++ src/Hoobot/Binance/orders.ts
@@ -29,8 +29,8 @@ import Binance from "node-binance-api";
 import { sendMessageToChannel } from "../../Discord/discord";
 import { Client } from "discord.js";
 import { ConfigOptions } from "../Utilities/args";
-import { ConsoleLogger } from "../Utilities/consoleLogger";
-import { calculatePercentageDifference } from "./trade";
+import { ConsoleLogger, consoleLogger } from "../Utilities/consoleLogger";
+import { calculatePercentageDifference, calculateUnrealizedPNLPercentageForLong, calculateUnrealizedPNLPercentageForShort, delay } from "./trade";
 
 export interface Order {
   quoteQty: string;
@@ -84,56 +84,51 @@ export const cancelOrder = async (
 export const handleOpenOrders = async (
   discord: Client, 
   binance: Binance, 
-  oSymbol: string,
-  openOrders: any[], 
+  symbol: string,
   orderBook: any, 
   options: ConfigOptions,
-  consoleLogger: ConsoleLogger, 
 ): Promise<string> => {
-  const currentTime = Date.now();
-  for (const order of openOrders) {
-    const { orderId, symbol, time, side, status, price } = order;
-    if (oSymbol.split("/").join("") !== symbol.split("/").join("")) {
-      continue;
-    }
-    const orderAgeSeconds = Math.floor((currentTime - time) / 1000);
-    consoleLogger.push("Order ID: ", orderId);
-    consoleLogger.push("Symbol: ", symbol);
-    consoleLogger.push("Age seconds: ", orderAgeSeconds);
-    if (orderAgeSeconds > options.maxOrderAge) {
-      await cancelOrder(binance, symbol, orderId);
-      const orderMsg = `>>> Order ID **${orderId}** for symbol **${symbol.split("/").join("")}** cancelled due to exceeding max age ${options.maxOrderAge} seconds.\nTime now ${new Date().toLocaleString("fi-fi")}\n`;
-      sendMessageToChannel(discord, options.discordChannelID, orderMsg);
-      consoleLogger.push("order-msg", orderMsg);
-      return "canceled";
-    } else {
-      if (side === "BUY") {
-        const orderBookBids = Object.keys(orderBook.bids).map(price => parseFloat(price)).sort((a, b) => a - b);
-        const bid = orderBookBids[orderBookBids.length - 1];
-        const diff = Math.abs(calculatePercentageDifference(bid, price));
-        if (diff > options.closePercentage) {
-          await cancelOrder(binance, symbol, orderId);
-          const orderMsg = `>>> Order ID **${orderId}** for symbol **${symbol.split("/").join("")}** cancelled due to price has changed over risk percentage ${options.closePercentage.toFixed(2)}%, difference between ${bid} bid and current ${price} order price ${diff.toFixed(4)}.\nTime now ${new Date().toLocaleString("fi-fi")}\n`;
-          sendMessageToChannel(discord, options.discordChannelID, orderMsg);
-          consoleLogger.push("order-msg", orderMsg);
-          return "canceled";
+  let openOrders: Order[] = []
+  do {
+    openOrders = await binance.openOrders(symbol.split("/").join(""));
+    if (openOrders.length > 0) {
+      for (const order of openOrders) {
+        if (symbol.split("/").join("") !== order.symbol.split("/").join("")) {
+          continue;
         }
-      } else {
-        const orderBookAsks = Object.keys(orderBook.asks).map(price => parseFloat(price)).sort((a, b) => a - b);
-        const ask = orderBookAsks[0];
-        const diff = Math.abs(calculatePercentageDifference(ask, price));
-        if (diff > options.closePercentage) {
-          await cancelOrder(binance, symbol, orderId);
-          const orderMsg = `>>> Order ID **${orderId}** for symbol **${symbol.split("/").join("")}** cancelled due to price has changed over risk percentage ${options.closePercentage.toFixed(2)}%, difference between ${ask} ask and current ${price} order price ${diff.toFixed(4)}.\nTime now ${new Date().toLocaleString("fi-fi")}\n`;
+        const logger = consoleLogger();
+        const currentTime = Date.now();
+        const orderAgeSeconds = Math.floor((currentTime - order.time) / 1000);
+        logger.push("Order ID: ", order.orderId);
+        logger.push("Symbol: ", symbol);
+        logger.push("Age seconds: ", orderAgeSeconds);
+        if (orderAgeSeconds > options.maxOrderAge) {
+          await cancelOrder(binance, symbol, order.orderId);
+          const orderMsg = `>>> Order ID **${order.orderId}**\nSymbol **${symbol.split("/").join("")}**\nCancelled due to exceeding max age ${options.maxOrderAge} seconds.\nTime now ${new Date().toLocaleString("fi-fi")}\n`;
           sendMessageToChannel(discord, options.discordChannelID, orderMsg);
-          consoleLogger.push("order-msg", orderMsg);
+          logger.push("order-msg", orderMsg);
           return "canceled";
+        } else {
+          let unrealizedPNL = 0;
+          const orderBookAsks = Object.keys(orderBook.asks).map(price => parseFloat(price)).sort((a, b) => a - b);
+          const orderBookBids = Object.keys(orderBook.bids).map(price => parseFloat(price)).sort((a, b) => b - a);
+          if (order.isBuyer === true) { 
+            unrealizedPNL = calculateUnrealizedPNLPercentageForShort(parseFloat(order.quoteQty), parseFloat(order.price), orderBookAsks[0]);
+          } else { 
+            unrealizedPNL = calculateUnrealizedPNLPercentageForLong(parseFloat(order.quoteQty), parseFloat(order.price), orderBookBids[0]);
+          }
+          if (unrealizedPNL < options.closePercentage) {
+            await cancelOrder(binance, symbol, order.orderId);
+            const orderMsg = `>>> Order ID **${order.orderId}**\n symbol **${symbol.split("/").join("")}**\nCancelled due to risk percentage ${options.closePercentage.toFixed(2)}%\nCurrent bid ${orderBookBids[0]}\nCurrent ask ${orderBookAsks[0]}\nCurrent price: ${order.price}\nDifference: ${unrealizedPNL.toFixed(4)} UPNL%.\nTime now ${new Date().toLocaleString("fi-fi")}\n`;
+            sendMessageToChannel(discord, options.discordChannelID, orderMsg);
+          }
         }
+        logger.print();
+        logger.flush();
       }
     }
-  }
-  consoleLogger.print();
-  consoleLogger.flush();
+    delay(1500);
+  } while(openOrders.length > 0);
 };
 
 export const getLastCompletedOrder = async (binance: Binance, pair: string): Promise<Order> => {
