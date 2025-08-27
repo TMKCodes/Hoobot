af1cddab81cbbf4fe7890e229e0eba8cc8a810a5
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2025-08-27 17:45:48 +0300

    Add checks and more information that manual trade needs to be done for the symbol.

--- src/Hoobot/Indicators/Balance.ts
+++ src/Hoobot/Indicators/Balance.ts
@@ -1,29 +1,29 @@
 /* =====================================================================
-* Hoobot - Proprietary License
-* Copyright (c) 2023 Hoosat Oy. All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are not permitted without prior written permission
-* from Hoosat Oy. Unauthorized reproduction, copying, or use of this
-* software, in whole or in part, is strictly prohibited. All 
-* modifications in source or binary must be submitted to Hoosat Oy in source format.
-*
-* THIS SOFTWARE IS PROVIDED BY HOOSAT OY "AS IS" AND ANY EXPRESS OR
-* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-* ARE DISCLAIMED. IN NO EVENT SHALL HOOSAT OY BE LIABLE FOR ANY DIRECT,
-* INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-* STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
-* OF THE POSSIBILITY OF SUCH DAMAGE.
-*
-* The user of this software uses it at their own risk. Hoosat Oy shall
-* not be liable for any losses, damages, or liabilities arising from
-* the use of this software.
-* ===================================================================== */
+ * Hoobot - Proprietary License
+ * Copyright (c) 2023 Hoosat Oy. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are not permitted without prior written permission
+ * from Hoosat Oy. Unauthorized reproduction, copying, or use of this
+ * software, in whole or in part, is strictly prohibited. All
+ * modifications in source or binary must be submitted to Hoosat Oy in source format.
+ *
+ * THIS SOFTWARE IS PROVIDED BY HOOSAT OY "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL HOOSAT OY BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The user of this software uses it at their own risk. Hoosat Oy shall
+ * not be liable for any losses, damages, or liabilities arising from
+ * the use of this software.
+ * ===================================================================== */
 
 import { ConsoleLogger } from "../Utilities/ConsoleLogger";
 import { ExchangeOptions, SymbolOptions } from "../Utilities/Args";
@@ -31,49 +31,63 @@ import { Filter } from "../Exchanges/Filters";
 import { checkPreviousTrade } from "../Exchanges/Trades";
 
 export const checkBalanceSignals = (
-  consoleLogger: ConsoleLogger, 
+  consoleLogger: ConsoleLogger,
   symbol: string,
-  closePrice: number,  
+  closePrice: number,
   exchangeOptions: ExchangeOptions,
-  filter: Filter,
+  filter: Filter
 ) => {
-  let check = 'HOLD';
+  let check = "HOLD";
   if (exchangeOptions.balances !== undefined) {
+    if (exchangeOptions.balances[symbol.split("/")[0]] == undefined) {
+      consoleLogger.push("Trades", {
+        error: `Can not find balance for ${symbol.split("/")[0]}.`,
+        next: `Most likely need manual trade as starting point for the symbol.`,
+      });
+      return check;
+    }
+    if (exchangeOptions.balances[symbol.split("/")[1]] == undefined) {
+      consoleLogger.push("Trades", {
+        error: `Can not find balance for ${symbol.split("/")[1]}.`,
+        next: `Most likely need manual trade as starting point for the symbol.`,
+      });
+      return check;
+    }
     const baseBalance = exchangeOptions.balances[symbol.split("/")[0]].crypto;
     const quoteBalance = exchangeOptions.balances[symbol.split("/")[1]].crypto;
-    const baseBalanceConverted = (baseBalance * closePrice);
+    const baseBalanceConverted = baseBalance * closePrice;
     let tradeCheck = checkPreviousTrade(symbol, exchangeOptions);
-    if (tradeCheck === 'SELL') {
+    if (tradeCheck === "SELL") {
       if (quoteBalance > filter.minNotional) {
-        check = 'BUY';
+        check = "BUY";
       } else {
         if (baseBalanceConverted >= filter.minNotional) {
-          check = 'SELL';
+          check = "SELL";
         } else {
-          check = 'HOLD';
+          check = "HOLD";
         }
       }
-    } else if (tradeCheck === 'BUY') {
+    } else if (tradeCheck === "BUY") {
       if (baseBalanceConverted > filter.minNotional) {
-        check = 'SELL'
+        check = "SELL";
       } else {
         if (quoteBalance >= filter.minNotional) {
-          check = 'BUY';
+          check = "BUY";
         } else {
-          check = 'HOLD';
+          check = "HOLD";
         }
       }
     }
-    if (check === 'SELL' && (baseBalanceConverted < filter.minNotional || baseBalanceConverted > filter.maxNotional)) {
-      check = 'HOLD';
-    } else if (check === 'BUY' && (quoteBalance < filter.minNotional || quoteBalance > filter.maxNotional)) {
-      check = 'HOLD';
+    if (check === "SELL" && (baseBalanceConverted < filter.minNotional || baseBalanceConverted > filter.maxNotional)) {
+      check = "HOLD";
+    } else if (check === "BUY" && (quoteBalance < filter.minNotional || quoteBalance > filter.maxNotional)) {
+      check = "HOLD";
     }
     consoleLogger.push("Trades", {
       previous: tradeCheck,
       next: check,
-    })
+    });
   }
-  
+
   return check;
-}
+};
