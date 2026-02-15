85794f3a0abff01308bf02c6b0bd3cc28b48ac6d
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2026-02-15 22:09:27 +0200

    Remove license and publish link and update version

--- src/Hoobot/Utilities/License.ts
+++ src/Hoobot/Utilities/License.ts
@@ -29,5 +29,6 @@ import { logToFile } from "./LogToFile";
 
 // Function to check if the provided license key is valid
 export const checkLicenseValidity = async (license: string): Promise<boolean> => {
+  console.log(`License: ${license}`);
   return true;
 };
