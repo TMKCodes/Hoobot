bf6576553b48be5d64370e20b16cd4c78cce9b07
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2026-02-16 18:11:08 +0200

    Update the Proprietary license to MIT.

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,3 @@
-/* =====================================================================
- * Hoobot - Proprietary License
- * Copyright (c) 2023 Hoosat Oy. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are not permitted without prior written permission
- * from Hoosat Oy. Unauthorized reproduction, copying, or use of this
- * software, in whole or in part, is strictly prohibited. All
- * modifications in source or binary must be submitted to Hoosat Oy in source format.
- *
- * THIS SOFTWARE IS PROVIDED BY HOOSAT OY "AS IS" AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL HOOSAT OY BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * The user of this software uses it at their own risk. Hoosat Oy shall
- * not be liable for any losses, damages, or liabilities arising from
- * the use of this software.
- * ===================================================================== */
-
 import fs from "fs";
 import Binance from "node-binance-api";
 import { loginDiscord } from "./Discord/discord";
